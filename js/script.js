9ced8ff976f3e6ea06fe8fe89660e11643fdf67f
diehlms <diehlstx@gmail.com>
2019-07-07 11:52:02 -0500

    added cards for sites

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ var instance = M.Carousel.init({
     fullWidth: true,
     indicators: true
 });
+
+window.onscroll = function() { scroll()};
+
+let navbar = document.getElementById("navbar");
+
+let sticky = navbar.offsetTop;
+
+function scroll() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky")
+    }
+}
