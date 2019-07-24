c46860d290464a49dc8ece11c734a9dece286d43
diehlms <diehlstx@gmail.com>
2019-07-23 22:50:22 -0500

    mobile styling

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
 M.AutoInit();
 
-var instance = M.Carousel.init({
-    fullWidth: true,
-    indicators: true
-});
-
 window.onscroll = function() { scroll()};
 
 let navbar = document.getElementById("navbar");
 
 let sticky = navbar.offsetTop;
+
 
 function scroll() {
     if (window.pageYOffset >= sticky) {
@@ -18,3 +14,17 @@ function scroll() {
         navbar.classList.remove("sticky")
     }
 }
+
+// window.onscroll = function() { titleScroll()};
+
+// let title = document.getElementById("title");
+
+// let titleSticky = title.offsetTop;
+
+// function titleScroll() {
+//     if (window.pageYOffset >= titleSticky && window.pageYOffset > sticky) {
+//         title.classList.add("sticky");
+//     } else {
+//         title.classList.remove("sticky");
+//     }
+// }
