ebdba00919ab205671cba547d2ee61964a8c91cc
diehlms <diehlstx@gmail.com>
2019-07-24 13:54:20 -0500

    mobile styling

--- js/script.js
+++ js/script.js
@@ -2,16 +2,22 @@ M.AutoInit();
 
 window.onscroll = function() { scroll()};
 
+
 let navbar = document.getElementById("navbar");
+let title = document.getElementById("navbar-lead");
 
 let sticky = navbar.offsetTop;
 
 
 function scroll() {
     if (window.pageYOffset >= sticky) {
+        title.style.display = "block";
+        title.style.fontFamily = "Rubik Mono One, sans-serif"
+        title.style.fontSize = "10px"
         navbar.classList.add("sticky");
     } else {
-        navbar.classList.remove("sticky")
+        title.style.display = "none";
+        navbar.classList.remove("sticky");
     }
 }
 
